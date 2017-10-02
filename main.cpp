2c533bade24adf3ec7c79ebc50428d1e8bac895f
Thales Sabino <thales@codeplay.com>
2017-10-02 17:42:23 +0100

    Printing more physical device properties

--- main.cpp
+++ main.cpp
@@ -23,41 +23,51 @@ void PrintKeyValuePair(KeyType key, ValueType value)
     std::cout << key << " : " << value << std::endl;
 }
 
+#define VK_PRINT_PROPERTY(prop) PrintKeyValuePair(#prop, prop);
+
 int main(int argc, const char * argv[])
 {
     VkApplicationInfo appInfo = {};
     appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
     appInfo.pApplicationName = "Playing with Vulkan";
     appInfo.apiVersion = VK_MAKE_VERSION(1, 0, 0);
-    
+
     VkInstanceCreateInfo instanceCreateInfo = {};
     instanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
     instanceCreateInfo.pApplicationInfo = &appInfo;
-    
+
     VkInstance instance;
     VK_RESULT_CHECK(vkCreateInstance(&instanceCreateInfo, nullptr, &instance));
-    
+
     uint32_t physicalDeviceCount = 0;
     VK_RESULT_CHECK(vkEnumeratePhysicalDevices(instance, &physicalDeviceCount, nullptr));
-    
-    if(physicalDeviceCount <= 0)
+
+    if (physicalDeviceCount <= 0)
     {
         std::cout << "No physical device available" << std::endl;
         return EXIT_FAILURE;
     }
-    
+
     std::vector<VkPhysicalDevice> physicalDevices{ physicalDeviceCount };
     VK_RESULT_CHECK(vkEnumeratePhysicalDevices(instance, &physicalDeviceCount, physicalDevices.data()));
-    
-    for(const auto& physicalDevice : physicalDevices)
+
+    for (const auto& physicalDevice : physicalDevices)
     {
-        VkPhysicalDeviceProperties props = {};
-        vkGetPhysicalDeviceProperties(physicalDevice, &props);
-        
-        PrintKeyValuePair("Device Name", props.deviceName);
+        VkPhysicalDeviceProperties deviceProps = {};
+        vkGetPhysicalDeviceProperties(physicalDevice, &deviceProps);
+
+        VK_PRINT_PROPERTY(deviceProps.deviceID);
+        VK_PRINT_PROPERTY(deviceProps.deviceName);
+        VK_PRINT_PROPERTY(deviceProps.deviceType);
+        VK_PRINT_PROPERTY(deviceProps.driverVersion);
+        VK_PRINT_PROPERTY(deviceProps.apiVersion);
+        VK_PRINT_PROPERTY(deviceProps.limits.maxFramebufferWidth);
+        VK_PRINT_PROPERTY(deviceProps.limits.maxFramebufferHeight);
+
+        PrintKeyValuePair("Device Name", deviceProps.deviceName);        
     }
 
     vkDestroyInstance(instance, nullptr);
-    
+
     return EXIT_SUCCESS;
 }
